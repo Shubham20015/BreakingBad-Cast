f80c184a080337576ccec055e98e251f97f8a451
Shubham <Shubham20015>
2021-10-17 13:24:03 +0530

    Initial commit

---
+++ src/components/CharacterItem.js
@@ -0,0 +1,34 @@
+const CharacterItem = ({ item }) => {
+  return (
+    <div className="card">
+      <div className="card-inner">
+        <div className="card-front">
+          <img src={item.img} alt="Person image" />
+        </div>
+        <div className="card-back">
+          <h1>{item.name}</h1>
+          <ul>
+            <li>
+              <strong>Actor Name:</strong> {item.portrayed}
+            </li>
+            <li>
+              <strong>Nickname:</strong> {item.nickname}
+            </li>
+            <li>
+              <strong>Character Occupation:</strong>{" "}
+              {item.occupation.toString()}
+            </li>
+            <li>
+              <strong>Birthday:</strong> {item.birthday}
+            </li>
+            <li>
+              <strong>Status:</strong> {item.status || "Live Long Forever"}
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CharacterItem;
